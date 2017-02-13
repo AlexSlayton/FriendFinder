a2a9fe8a9d67959f8123c6424abe58d961544994
Alex Slayton <alexslayton@Alexs-MacBook-Pro.local>
2017-02-13 14:33:01 -0600

    saving with some working code

--- server.js
+++ server.js
@@ -19,66 +19,33 @@ var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'AlexJenn1',
-	database: 'hotRestaurant'
+	database: 'friendFinder'
 });
 
 // connect to the server
 connection.connect();
 
-var tables = [
-	
-];
 
-var waitingList = [
+var friendList = [
 
 ];
 
-// when this route is visited, send the browser to HotRestaurant/index.html
+// when this route is visited, send the browser to /index.html
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/reserve', function(req, res) {
-	res.sendFile(path.join(__dirname, "reserve.html"));
-});
-
-app.get('/tables', function(req, res) {
-	res.sendFile(path.join(__dirname, "tables.html"));
+app.get('/questions', function(req, res) {
+	res.sendFile(path.join(__dirname, "questions.html"));
 });
 
 // when /api/tables is visited, display the tables array
-app.get('/api/tables', function(req, res) {
-	connection.query('SELECT * FROM tables', function(err, result) {
+app.get('/api/friendList', function(req, res) {
+	connection.query('SELECT * FROM friendList', function(err, result) {
 		if (err) {
 			throw err;
 		}
 		res.send(result);
-	});
-});
-
-// when /api/waitingList is visited, display the waiting list array
-app.get('/api/waitingList', function(req, res) {
-	connection.query('SELECT * FROM waitingList', function(err, result) {
-		if (err) {
-			throw err;
-		}
-		res.send(result);
-	});
-});
-
-// when clearTables is visited, it clears tables and waitingList
-app.post('/clearTables', function(req, res) {
-	connection.query('DELETE FROM tables', function(err, result) {
-		if (err) {
-			throw err;
-		}
-		console.log("Cleared the Reservation table :D");
-	});
-	connection.query('DELETE FROM waitingList', function(err, result) {
-		if (err) {
-			throw err;
-		}
-		console.log("Cleared the Waiting List table :D");
 	});
 });
 
@@ -90,16 +57,11 @@ app.post('/makeReservation', function(req, res) {
 		}
 		console.log(req.body);
 		if (parseInt(result[0].count) < 5) {
-			connection.query('INSERT INTO tables (name, phone_number, email, unique_id) VALUES (?, ?, ?, ?)', [req.body.name, req.body.phone, req.body.email, req.body.unique], function(err, insertResult) {
-				res.send({reservation: "reservation"});
+			connection.query('INSERT INTO tables (nameFriend, photo, score, unique_id) VALUES (?, ?, ?, ?)', [req.body.name, req.body.phone, req.body.email, req.body.unique], function(err, insertResult) {
+				res.send({friendList: "friendList"});
 			});
 		}
-		else {
-			connection.query('INSERT INTO waitingList (name, phone_number, email, unique_id) VALUES (?, ?, ?, ?)', [req.body.name, req.body.phone, req.body.email, req.body.unique], function(err, insertResult) {
-				console.log("Added your name to the waiting list");
-				res.send({waiting: "waiting"});
-			});	
-		}
+
 	});
 });
 
